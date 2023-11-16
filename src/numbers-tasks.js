0c0f0233e2581625ce281572778f2a3ef582adf2
vladimir <vovchikbura@mail.ru>
2023-11-16 13:19:34 +0300

    feat: created isPrime function

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -225,8 +225,18 @@ function roundToPowerOfTen(num, pow) {
  *   16 => false
  *   17 => true
  */
-function isPrime(/* n */) {
-  throw new Error('Not implemented');
+function isPrime(n) {
+  // throw new Error('Not implemented');
+  let answer = true;
+
+  let i = 1;
+  while (i < n / 2) {
+    i += 1;
+    if (n % i === 0) answer = false;
+  }
+
+  console.log('answer: ', answer);
+  return answer;
 }
 
 /**
